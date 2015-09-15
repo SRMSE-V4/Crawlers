e9c42afab97b4543d37a94e1086a0607daa12b4c
rishavmedhi <rishav164@gmail.com>
2015-09-15 13:53:56 +0530

    crawler for world mountains

--- mountains.py
+++ mountains.py
@@ -1,4 +1,4 @@
-#x=filter(lambda x:ord(x)>31 and ord(x)<128,i.text)
+#crawler for crawling world mountains from wikipedia
 import pymongo
 import urllib2
 from bs4 import BeautifulSoup
@@ -7,47 +7,98 @@ client=pymongo.MongoClient()
 mdb=client['brainse']
 qb=mdb['mountains']
 
+def image_search(rdata):
+	link=rdata["Link"]
+	pg=urllib2.urlopen(link).read()
+	soup=BeautifulSoup(pg)
+	info=soup.find("table",class_="infobox vcard")
+	img=info.find_all("img")
+	if len(img)!=0:
+		rdata["display_order"].append("Image")
+		rdata["Image"]=" https:"+img[0].get('src')+" "
+		#print rdata["Image"]
+		return rdata
+	else:
+		return rdata
+		
+#function for processing table 1 and 3
 def process_table(tb1):
-	#print "here"
-	rdata={}
 	row=tb1.find_all("tr")
 	del row[0]
 	for i in row:
-		data=i.find_all("td")
-		#print data
-		rdata["Name"]=" "+data[0].find('a').get_text()+" "
-		if data[0].find("a")!=None:
-			rdata["Link"]=" https://en.wikipedia.org"+data[0].find("a").get('href')+" "
-		rdata["Metres"]=" "+data[1].get_text()+" "
-		rdata["Feet"]=" "+data[2].get_text()+" "
-		x=filter(lambda x:ord(x)>31 and ord(x)<128,data[3].get_text())
-		rdata["Range"]=" "+str(x)+" "
-		x=filter(lambda x:ord(x)>31 and ord(x)<128,data[4].get_text())
-		rdata["Location and Description"]=" "+str(x)+" "
-		#print data[0]
-		#rdata["Name"]=data[0].find('a').get_text()
-		print rdata["Name"]
-		#count=count+1	
-
+		try:
+			rdata={}
+			data=i.find_all("td")
+			rdata["Name"]=" "+data[0].find('a').get_text()+" "
+			if data[0].find("a")!=None:
+				rdata["Link"]=" https://en.wikipedia.org"+data[0].find("a").get('href')+" "
+			rdata["Metres"]=" "+data[1].get_text()+" "
+			rdata["Feet"]=" "+data[2].get_text()+" "
+			x=filter(lambda x:ord(x)>31 and ord(x)<128,data[3].get_text())
+			rdata["Range"]=" "+str(x)+" "
+			rdata["Range"]=rdata["Range"].replace(";","")
+			x=filter(lambda x:ord(x)>31 and ord(x)<128,data[4].get_text())
+			rdata["Location and Description"]=" "+str(x)+" "
+			rdata["display_order"]=["Name","Metres","Feet","Range","Location and Description","Link"]
+			print rdata["Name"]
+			rdata=image_search(rdata)
+			qb.insert(rdata)
+		except Exception as e:
+			print e
+			pass
+			
+#function for processing rest of the tables excluding the last one
 def process_table1(tb):
-	rdata={}
 	row=tb.find_all("tr")
 	del row[0]
 	for i in row:
-		data=i.find_all("td")
-		#print data
-		if data[0].find('a')!=None:
-			rdata["Name"]=" "+data[0].find('a').get_text()+" "
-		else:
-			rdata["Name"]=" "+str(data[0].get_text())+" "
-		if data[0].find("a")!=None:
-			rdata["Link"]=" https://en.wikipedia.org"+data[0].find("a").get('href')+" "
-		rdata["Metres"]=" "+data[1].get_text()+" "
-		rdata["Feet"]=" "+data[2].get_text()+" "
-		x=filter(lambda x:ord(x)>31 and ord(x)<128,data[3].get_text())
-		rdata["Location and Description"]=" "+str(x)+" "
-		print rdata["Name"]
-rdata={}
+		try:
+			rdata={}
+			data=i.find_all("td")
+			if data[0].find('a')!=None:
+				rdata["Name"]=" "+data[0].find('a').get_text()+" "
+			else:
+				rdata["Name"]=" "+str(data[0].get_text())+" "
+			if data[0].find("a")!=None:
+				rdata["Link"]=" https://en.wikipedia.org"+data[0].find("a").get('href')+" "
+			rdata["Metres"]=" "+data[1].get_text()+" "
+			rdata["Feet"]=" "+data[2].get_text()+" "
+			x=filter(lambda x:ord(x)>31 and ord(x)<128,data[3].get_text())
+			rdata["Location and Description"]=" "+str(x)+" "
+			rdata["display_order"]=["Name","Metres","Feet","Location and Description","Link"]
+			print rdata["Name"]
+			rdata=image_search(rdata)
+			qb.insert(rdata)
+		except Exception as e:
+			print e
+			pass
+
+#function for crawling last table
+def process_table2(tb):
+	row=tb.find_all("tr")
+	del row[0]
+	for i in row:
+		try:
+			rdata={}
+			data=i.find_all("td")
+			if data[0].find('a')!=None:
+				rdata["Name"]=" "+data[0].find('a').get_text()+" "
+			else:
+				rdata["Name"]=" "+str(data[0].get_text())+" "
+			if data[0].find("a")!=None:
+				rdata["Link"]=" https://en.wikipedia.org"+data[0].find("a").get('href')+" "
+			rdata["Metres"]=" "+data[1].get_text()+" "
+			rdata["Feet"]=" "+data[2].get_text()+" "
+			x=filter(lambda x:ord(x)>31 and ord(x)<128,data[4].get_text())
+			rdata["Location and Description"]=" "+str(x)+" "
+			rdata["display_order"]=["Name","Metres","Feet","Location and Description","Link"]
+			print rdata["Name"]
+			rdata=image_search(rdata)
+			qb.insert(rdata)
+		except Exception as e:
+			print e
+			pass
+
 count=0
 url="https://en.wikipedia.org/wiki/List_of_mountains_by_elevation"
 pg=urllib2.urlopen(url).read()
@@ -63,4 +114,4 @@ process_table1(tb1[3])
 process_table1(tb1[4])
 process_table1(tb1[5])
 process_table1(tb1[6])
-process_table1(tb1[7])
+process_table2(tb1[7]) 
